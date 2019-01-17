29013b57fe6ca9159f1c99eca49d669abb9b2fa0
wlovins <wlovins@gmail.com>
2019-01-17 13:31:31 +0000

    Update build_windows_10.sh

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-packer build --only=vmware-iso --var iso_url=~D:/src/SW_DVD9_Win_Pro_Ent_Edu_N_10_1803_64BIT_Eng_Intl_-4_MLF_X21-87131.ISO windows_10.json
+packer build --only=vmware-iso --var iso_url=D:/src/SW_DVD9_Win_Pro_Ent_Edu_N_10_1803_64BIT_Eng_Intl_-4_MLF_X21-87131.ISO windows_10.json
 
