c1b1c307e44c433378b045c39b11764dcea68f38
wlovins <wlovins@gmail.com>
2019-01-17 13:31:20 +0000

    Update build_windows_10.sh

--- build_windows_10.sh
+++ build_windows_10.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-packer build --only=vmware-iso --var iso_url=~/Downloads/17763.1.180914-1434.rs5_release_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso windows_10.json
+packer build --only=vmware-iso --var iso_url=~D:/src/SW_DVD9_Win_Pro_Ent_Edu_N_10_1803_64BIT_Eng_Intl_-4_MLF_X21-87131.ISO windows_10.json
 
